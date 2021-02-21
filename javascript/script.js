fcb7221bb7cea9e428e51516d2ca2a83aa2e98f8
judas1221 <judasaor@gmail.com>
2021-02-21 13:33:14 -0500

    primeros eventos

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+let modoOscuro = document.getElementById("modooscuro");
+modoOscuro.addEventListener('click',() => {
+    modoOscuro.style.color ="#9dafc4";
+});
+let favoritos = document.getElementById("favoritos");
+favoritos.addEventListener('click',() => {
+    favoritos.style.color ="#9dafc4";
+});
+let misGifs = document.getElementById('misgifs');
+misGifs.addEventListener('click',() => {
+    misGifs.style.color ="#9dafc4";
+});
+let camara = document.getElementById("camara");
+camara.addEventListener('click',() => {
+    camara.style.backgroundColor="#9dafc4";
+});
