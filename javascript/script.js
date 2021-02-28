25fa8a870a5815322f7f24a8f5c9918eb2c248c9
judas1221 <judasaor@gmail.com>
2021-02-28 12:01:19 -0500

    modo oscuro

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// Cambiar el color al seleccionar ul menu nav
 let modoOscuro = document.getElementById("modooscuro");
 modoOscuro.addEventListener('click',() => {
     modoOscuro.style.color ="#9dafc4";
@@ -14,3 +15,27 @@ let camara = document.getElementById("camara");
 camara.addEventListener('click',() => {
     camara.style.backgroundColor="#9dafc4";
 });
+
+// Cambiar el color a modo oscuro
+modoOscuro.addEventListener('click', cambiarModo);
+
+function cambiarModo() { 
+    let cuerpoweb = document.body; 
+    cuerpoweb.classList.toggle("oscuro"); 
+}
+
+//Acceder a la camara
+function getStreamAndRecord () { 
+    navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+       height: { max: 480 }
+    }
+ })
+ .then(function(stream) {
+    video.srcObject = stream;
+    video.play()
+})};
+
+let activarCamara = document.getElementById("comenzar");
+activarCamara.addEventListener('click', getStreamAndRecord);
